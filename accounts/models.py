26b08708a886e7cc4e59076ac9884fc8190bd7ff
JaafarAS <jaafar.blog.com>
2020-09-18 20:22:51 +0300

    add profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    USER_TYPE =[
+        ('admin','اداري'),
+        ('teacher','استاذ'), 
+        ('student', 'طالب'),
+        ]
+    user_type = models.CharField(max_length=10, choices=USER_TYPE)
+    phone_number = models.CharField(max_length=11, null=True , blank=True )
+
+
+    def __str__(self):
+        return str(self.user)
+
